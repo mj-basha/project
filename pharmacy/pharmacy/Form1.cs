bbac4b0f6d1839b15fa46dcbbd22aec3c26d82b5
mj-basha <55tayho9@gmail.com>
2025-05-19 20:11:11 +0200

    Update Form1.cs

--- pharmacy/pharmacy/Form1.cs
+++ pharmacy/pharmacy/Form1.cs
@@ -13,72 +13,13 @@ namespace pharmacy
 {
     public partial class Form1 : Form
     {
-        private DataTable medicineTable = new DataTable();
 
         public Form1()
         {
             InitializeComponent();
-            InitializeMedicineTable();
-            BindGrid();
+            
 
         }
 
-        private void BindGrid()
-        {
-            dataGridViewMedicines.DataSource = medicineTable;
         }
-
-
-        private void btnAdd_Click(object sender, EventArgs e)
-        {
-            medicineTable.Rows.Add(
-            txtMedicineID.Text,
-               txtName.Text,
-               txtManufacturer.Text,
-               txtCategory.Text,
-               txtQuantity.Text,
-               txtUnitPrice.Text,
-               dtpExpiryDate.Value.ToShortDateString()
-           );
-            ClearFields();
-
-        }
-
-        private void btnUpdate_Click(object sender, EventArgs e)
-        {
-            if (dataGridViewMedicines.SelectedRows.Count > 0)
-            {
-                var row = dataGridViewMedicines.SelectedRows[0];
-                row.Cells["MedicineID"].Value = txtMedicineID.Text;
-                row.Cells["Name"].Value = txtName.Text;
-                row.Cells["Manufacturer"].Value = txtManufacturer.Text;
-                row.Cells["Category"].Value = txtCategory.Text;
-                row.Cells["Quantity"].Value = txtQuantity.Text;
-                row.Cells["UnitPrice"].Value = txtUnitPrice.Text;
-                row.Cells["ExpiryDate"].Value = dtpExpiryDate.Value.ToShortDateString();
-                ClearFields();
-            }
-
-        }
-
-        private void btnDelete_Click(object sender, EventArgs e)
-        {
-            if (dataGridViewMedicines.SelectedRows.Count > 0)
-            {
-                dataGridViewMedicines.Rows.Remove(dataGridViewMedicines.SelectedRows[0]);
-                ClearFields();
-            }
-
-        }
-
-        private void btnSearch_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnCheckExpiry_Click(object sender, EventArgs e)
-        {
-
-        }
-    }
 }
